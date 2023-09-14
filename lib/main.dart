d4535d09266ac29d2f56657ea1b306d83375871a
LaXnZ <sumudithalanz@gmail.com>
2023-09-14 08:41:29 +0530

    fixed login screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: CustomAppTheme.lightTheme,
       darkTheme: CustomAppTheme.darkTheme,
       themeMode: ThemeMode.system,
--- lib/src/features/authentication/screens/login_screen/login_screen.dart
+++ lib/src/features/authentication/screens/login_screen/login_screen.dart
@@ -8,163 +8,199 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-            child: Container(
-      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 50),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Center(
-            child: Image(
-              image: AssetImage(kLoginLogo),
+      resizeToAvoidBottomInset: false,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double screenHeight = constraints.maxHeight;
+          return Container(
+            height: screenHeight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    kBackgroundImageLight), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 70,
-          ),
-          Text('Login',
-              style: TextStyle(
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold,
-                  color: const Color(kPrimaryBlackColor))),
-          const SizedBox(
-            height: 20,
-          ),
-          Form(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.mail_outline),
-                  labelText: 'Email',
-                  hintText: AutofillHints.email,
-                  hintStyle:
-                      TextStyle(fontSize: 13, color: Color(kPrimaryBlackColor)),
-                  labelStyle: TextStyle(
-                    fontSize: 14,
-                    color: Color(kPrimaryBlackColor),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.password),
-                  labelText: 'Password',
-                  hintText: AutofillHints.password,
-                  hintStyle:
-                      TextStyle(fontSize: 13, color: Color(kPrimaryBlackColor)),
-                  labelStyle: TextStyle(
-                    fontSize: 14,
-                    color: Color(kPrimaryBlackColor),
-                  ),
-                  suffixIcon: IconButton(
-                      onPressed: null, icon: Icon(Icons.remove_red_eye_sharp)),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {}, child: Text('Forgot Password ?'))),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: const Color(kPrimaryBlackColor),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                      onPressed: () {},
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 20),
-                      ))),
-            ],
-          )),
-          const SizedBox(
-            height: 30,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Or continue with',
-                style: TextStyle(fontSize: 15),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: 50,
-                width: double.infinity,
-                child: OutlinedButton.icon(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        const Color(kPrimaryBlueColor)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Adjust the radius as needed
+            child: SingleChildScrollView(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Center(
+                      child: Image(
+                        image: AssetImage(kLoginLogo),
                       ),
                     ),
-                  ),
-                  icon: Image(
-                    image: AssetImage(kGoogleLogo),
-                    width: 20.0,
-                  ),
-                  onPressed: () {},
-                  label: Text(
-                    'Google',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Color(kPrimaryWhiteColor),
+                    const SizedBox(
+                      height: 70,
                     ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: TextButton(
-                  onPressed: () {},
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Already Have an Account ? ',
-                      style: TextStyle(
-                        color: Colors
-                            .black, // Set the color for "Already Have an Account ?"
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'Sign Up',
-                          style: TextStyle(
+                    const Text('Login',
+                        style: TextStyle(
+                            fontSize: 45,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue, // Set the color for "Sign Up"
+                            color: Color(kPrimaryBlackColor))),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Color(kPrimaryBlackColor),
+                            ),
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.mail_outline),
+                              labelText: 'Email',
+                              hintText: AutofillHints.email,
+                              hintStyle: TextStyle(
+                                  fontSize: 13,
+                                  color: Color(kPrimaryBlackColor)),
+                              labelStyle: TextStyle(
+                                fontSize: 14,
+                                color: Color(kPrimaryBlackColor),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Color(kPrimaryBlackColor),
+                            ),
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.password),
+                              labelText: 'Password',
+                              hintText: AutofillHints.password,
+                              hintStyle: TextStyle(
+                                  fontSize: 13,
+                                  color: Color(kPrimaryBlackColor)),
+                              labelStyle: TextStyle(
+                                fontSize: 14,
+                                color: Color(kPrimaryBlackColor),
+                              ),
+                              suffixIcon: IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.remove_red_eye_sharp)),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Align(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Forgot Password ?'))),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: const Color(kPrimaryBlackColor),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                onPressed: () {},
+                                child: const Text(
+                                  'Login',
+                                  style: TextStyle(fontSize: 20),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Or continue with',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        SizedBox(
+                          height: 50,
+                          width: double.infinity,
+                          child: OutlinedButton.icon(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color(kPrimaryBlueColor)),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      8.0), // Adjust the radius as needed
+                                ),
+                              ),
+                            ),
+                            icon: const Image(
+                              image: AssetImage(kGoogleLogo),
+                              width: 20.0,
+                            ),
+                            onPressed: () {},
+                            label: const Text(
+                              'Google',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(kPrimaryWhiteColor),
+                              ),
+                            ),
                           ),
                         ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: RichText(
+                              text: const TextSpan(
+                                text: 'Already Have an Account ? ',
+                                style: TextStyle(
+                                  color: Colors
+                                      .black, // Set the color for "Already Have an Account ?"
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: 'Sign Up',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors
+                                          .blue, // Set the color for "Sign Up"
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                  ),
+                  ],
                 ),
-              )
-            ],
-          ),
-        ],
+              ),
+            ),
+          );
+        },
       ),
-    )));
+    );
   }
 }
